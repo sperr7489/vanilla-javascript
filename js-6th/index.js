d4a495efe7cdc335b66aa1373fb0cfa2f807d786
sperr7489 <sperr@ajou.ac.kr>
2021-04-28 13:35:15 +0900

    commit

---
+++ js-6th/index.js
@@ -0,0 +1,49 @@
+const range = document.querySelector(".input-range");
+const rangeInfo = document.querySelector(".range-number__info");
+const createBtn = document.querySelector(".random-number");
+const inputGuess = document.querySelector(".guess-number");
+const playBtn = document.querySelector(".result-check");
+const inputStatus = document.querySelector(".input-range__status");
+
+let machineVal;
+let rangeVal;
+function changeRange(event) {
+    rangeVal = range.value;
+    rangeInfo.innerText = `Generate a number between 0 and ${rangeVal}`;
+    machineVal = Math.floor(Math.random() * rangeVal);
+
+}
+function createNum(event) {
+    machineVal = Math.floor(Math.random() * rangeVal);
+    console.log("machineVal은" + machineVal);
+}
+
+function checkBlank() {
+    if (inputGuess.value === "") {
+        alert("예상하는 값을 입력하세요");
+    }
+}
+function checkResult(event) {
+    checkBlank();
+
+    if (inputGuess.value !== "") {
+
+        if (inputGuess.value === machineVal) {
+            inputStatus.children[0].innerText = `you Choose: ${inputGuess.value}, 
+            the Machine Choose: ${machineVal}`;
+            inputStatus.children[1].innerText = `You win!!!`;
+        } else {
+            inputStatus.children[0].innerText = ` you Choose: ${inputGuess.value},
+            the Machine Choose: ${machineVal}`;
+            inputStatus.children[1].innerText = `You lost!!!`;
+        }
+    }
+
+
+}
+
+range.addEventListener("change", changeRange);
+createBtn.addEventListener("click", createNum);
+playBtn.addEventListener("click", checkResult);
+
+
